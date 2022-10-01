750eb09733f121e4923b8ab647cf4298c8e46f1a
R8V <ignacio.slater@ug.uchile.cl>
2022-10-01 19:52:15 -0300

    FEAT implementate AbstractIngredient

--- src/main/kotlin/cl/uchile/dcc/drinks/Glass.kt
+++ src/main/kotlin/cl/uchile/dcc/drinks/Glass.kt
@@ -1,3 +1,24 @@
 package cl.uchile.dcc.drinks
 
-class Glass
+import cl.uchile.dcc.ingredients.Ingredient
+
+
+/**
+ * Represents a glass with [Ingredient]s.
+ *
+ * @property ingredients The [Ingredient]s in the glass.
+ * @property totalContent The total content of the glass in milliliters.
+ *
+ * @param _ingredients The [Ingredient]s in the glass.
+ * @constructor Creates a new glass with [Ingredient]s.
+ */
+class Glass(private var _ingredients: List<Ingredient>) {
+    private val ingredients: List<Ingredient>
+        get() = _ingredients.toList()
+    private val totalContent = 0
+
+    /** Empties the contents of the glass. */
+    fun emptyContent() {
+        // Do something uwu
+    }
+}
--- src/main/kotlin/cl/uchile/dcc/ingredients/Ingredient.kt
+++ src/main/kotlin/cl/uchile/dcc/ingredients/Ingredient.kt
@@ -1,6 +1,20 @@
 package cl.uchile.dcc.ingredients
 
+/**
+ * This file contains an interface to represent ingredients, and an abstract class to hold common
+ * functionality.
+ */
 
+/** An interface to represent ingredients.  */
 interface Ingredient
 
-abstract class AbstractIngredient : Ingredient
+/**
+ * An abstract class to hold common functionality for [Ingredient]s.
+ *
+ *  @property content   The content in milliliters.
+ *  @property ingredientName   The name of the ingredient.
+ */
+abstract class AbstractIngredient(private var content: Int, private val ingredientName: String) :
+        Ingredient {
+    override fun toString() = "$ingredientName{content=$content}"
+}
